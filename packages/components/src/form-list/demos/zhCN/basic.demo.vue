0818f78636d6032a056a4cde38367a4132310835
zhengchangfu <zhengchangfu@daojia-inc.com>
2024-07-14 22:35:56 +0800

    docs: 添加 demo

---
+++ packages/components/src/form-list/demos/zhCN/basic.demo.vue
@@ -0,0 +1,64 @@
+<markdown>
+  # 基本使用
+</markdown>
+
+<script lang="tsx">
+import { defineComponent } from 'vue'
+import { useProFormInstance } from 'pro-components-naive-ui'
+
+export default defineComponent({
+  setup() {
+    const [proFormInst, { submit }] = useProFormInstance()
+
+    function onSubmit(values: any) {
+      console.log(values, 'values')
+    }
+
+    return {
+      submit,
+      onSubmit,
+      proFormInst,
+    }
+  },
+})
+</script>
+
+<template>
+  <pro-form
+    ref="proFormInst"
+    @submit="onSubmit"
+  >
+    <pro-form-list
+      label="用户信息"
+      path="userInfo"
+    >
+      <pro-input
+        show-label="{{ $index === 0 }}"
+        label="姓名1"
+        path="nameExpression"
+      />
+      <pro-input
+        show-label="{{ $index === 0 }}"
+        label="姓名2"
+        path="nameSlot2"
+      />
+      <pro-select
+        show-label="{{ $index === 0 }}"
+        label="城市"
+        path="city"
+        class="w-150px"
+        visible="{{ !!$row.nameSlot }}"
+        required
+        :field-props="{
+          options: [
+            { label: '北京', value: 0 },
+            { label: '上海', value: 1 },
+          ],
+        }"
+      />
+    </pro-form-list>
+    <n-button type="primary" @click="submit">
+      登录
+    </n-button>
+  </pro-form>
+</template>
---
+++ packages/components/src/form-list/demos/zhCN/index.demo-entry.md
@@ -0,0 +1,19 @@
+# 结构化列表 ProFormList
+<!--single-column-->
+<n-flex vertical>
+    <n-alert type="success" title="提示" show-icon :bordered="false">
+    它也是一个表单控件，所以也支持控件的所有属性<br />
+  </n-alert>
+</n-flex>
+
+## 演示
+
+```demo
+basic.vue
+```
+
+## API
+### ProFormList 新增 Props
+
+| 名称 | 类型 | 默认值 | 说明 | 版本 |
+| --- | --- | --- | --- | --- |
