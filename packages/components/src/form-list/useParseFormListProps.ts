12ff2d699c65ae20f0860c7c3715d09511fa5fd1
zhengchangfu <zhengchangfu@daojia-inc.com>
2024-07-23 23:41:45 +0800

    chore: FormList useCompileFormListProps 调整为 useParseFormListProps

--- packages/components/src/form-list/useParseFormListProps.ts
+++ packages/components/src/form-list/useParseFormListProps.ts
@@ -1,17 +1,12 @@
 import type { ExpressionScope } from 'pro-components-hooks'
 import { useCompile } from 'pro-components-hooks'
-import { computed, toRef, useAttrs } from 'vue'
+import { toRef } from 'vue'
 import type { ProFormListProps } from './props'
 
-export function useCompileFormListProps(props: ProFormListProps, scope: ExpressionScope) {
-  const attrs = useAttrs()
+export function useParseFormListProps(props: ProFormListProps, scope: ExpressionScope) {
   const parsedMin = useCompile(toRef(props, 'min'), { scope })
   const parsedMax = useCompile(toRef(props, 'max'), { scope })
   const parsedPosition = useCompile(toRef(props, 'position'), { scope })
-  /**
-   * attrs 虽然是 proxy，但 isProxy 返回 false，所以浅克隆一层
-   */
-  const parsedAttrs = useCompile(computed(() => ({ ...attrs })), { scope })
   const parsedCopyButtonProps = useCompile(toRef(props, 'copyButtonProps'), { scope })
   const parsedRemoveButtonProps = useCompile(toRef(props, 'removeButtonProps'), { scope })
   const parsedCreatorButtonProps = useCompile(toRef(props, 'creatorButtonProps'), { scope })
@@ -19,7 +14,6 @@ export function useCompileFormListProps(props: ProFormListProps, scope: Expressi
   return {
     min: parsedMin,
     max: parsedMax,
-    attrs: parsedAttrs,
     position: parsedPosition,
     copyButtonProps: parsedCopyButtonProps,
     removeButtonProps: parsedRemoveButtonProps,
