49089a8dd379fb047e25c6ae9422c0eac36e98dc
zhengchangfu <zhengchangfu@daojia-inc.com>
2024-07-25 20:04:51 +0800

    chore: 删除fieldCoustomKeys无作用的属性

--- packages/components/src/form/field/fieldCustomKeys.ts
+++ packages/components/src/form/field/fieldCustomKeys.ts
@@ -1,27 +1,19 @@
-import type { FormItemInst, FormItemProps } from 'naive-ui'
+import type { FormItemInst } from 'naive-ui'
 import type { ComputedRef, Ref } from 'vue'
 
-interface NFormItemMeta {
-  path: FormItemProps['path']
-  label: FormItemProps['label']
-}
 export interface ProFieldConfig {
   /**
    * 组件名称
    */
   name: `Pro${string}`
   /**
-   * 诸如 input/select 等控件的值
+   * 组件值
    */
   value: ComputedRef<any>
   /**
    * 组件插槽
    */
   slots: Record<string, any>
-  /**
-   * n-form-item 的一些信息
-   */
-  nFormItemMeta: ComputedRef<NFormItemMeta>
   /**
    * n-form-item 的实例
    */
