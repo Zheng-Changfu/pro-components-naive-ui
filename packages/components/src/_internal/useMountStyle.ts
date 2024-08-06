55589cfa9dc207a801f1d9655a32cea9d57e1add
zhengchangfu <zhengchangfu@daojia-inc.com>
2024-08-06 21:58:28 +0800

    feat: 支持 ssr

---
+++ packages/components/src/_internal/useMountStyle.ts
@@ -0,0 +1,29 @@
+import type { CNode } from 'css-render'
+import { useSsrAdapter } from '@css-render/vue3-ssr'
+import { inject, onBeforeMount } from 'vue'
+
+export function useMountStyle(
+  mountId: string,
+  style: CNode | undefined,
+) {
+  const ssrAdapter = useSsrAdapter()
+  const cssrAnchorMetaName = 'naive-ui-style'
+  const NConfigProvider = inject<any>('n-config-provider', null)
+  if (style) {
+    const mountStyle = (): void => {
+      style.mount({
+        id: mountId,
+        head: true,
+        anchorMetaName: cssrAnchorMetaName,
+        ssr: ssrAdapter,
+        parent: NConfigProvider?.styleMountTarget,
+      })
+    }
+    if (ssrAdapter) {
+      mountStyle()
+    }
+    else {
+      onBeforeMount(mountStyle)
+    }
+  }
+}
