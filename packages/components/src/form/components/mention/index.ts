1c43ef8849f7d90b031b8675209d394d7ba43700
liyuan26 <liyuan26@daojia-inc.com>
2024-08-14 11:52:30 +0800

    feat: 新增 ProMention 组件

---
+++ packages/components/src/form/components/mention/index.ts
@@ -0,0 +1,5 @@
+export * from './inst'
+export * from './props'
+export * from './slots'
+export { default as ProMention } from './index.vue'
+export { default as ProFieldMention } from './fields/field-mention.vue'
