1314076ac0511fc3ca0b158ab288de2c6ce86bef
zhengchangfu <zhengchangfu@daojia-inc.com>
2024-08-17 00:43:10 +0800

    types: 类型优化

--- packages/components/src/form/components/field/slots.ts
+++ packages/components/src/form/components/field/slots.ts
@@ -2,23 +2,19 @@ import type { VNodeChild } from 'vue'
 
 export interface ProFieldSlots {
   /**
-   * 表单组件的后缀插槽
+   * 表单项的后缀插槽
    */
   'addon-after'?: any
   /**
-   * 表单组件的前缀插槽
+   * 表单项的前缀插槽
    */
   'addon-before'?: any
   /**
-   * 自定义渲染(表单+前后缀插槽)容器
+   * 自定义渲染(表单项+前后缀插槽)容器
    */
   'group'?: VNodeChild
   /**
-   * 自定义表单组件
-   */
-  'default'?: Record<string, any>
-  /**
-   * 表单组件只读模式下的内容
+   * 表单项只读模式下的内容
    */
   'readonly'?: Record<string, any>
   [x: string]: any
