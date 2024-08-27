c6bbfd493835fc3d5679a45c6c8b0ae3a25a9b9c
zhengchangfu <zcfpromise@163.com>
2024-08-27 23:55:48 +0800

    docs(ProDescriptions): 增加 demo

--- packages/components/src/descriptions/demos/zhCN/basic.demo.vue
+++ packages/components/src/descriptions/demos/zhCN/basic.demo.vue
@@ -1,47 +1,86 @@
 <markdown>
-# 基本使用
+# columns 配置
+
+`valueType` 代表的是组件类型，比如 `valueType` 为 `date`，会通过 `valueTypeMap` 找到组件，然后使用该组件将值进行格式化后展示，默认为 `input`，
+考虑到包体积问题，`valueTypeMap` 需用户手动在全局提供，[详情查看](config-provider)
 </markdown>
 
 <script lang="tsx">
 import { defineComponent } from 'vue'
 import type { ProDescriptionColumns } from 'pro-components-naive-ui'
 
-function delay(time: number) {
-  return new Promise(resolve => setTimeout(resolve, time))
+interface DataSource {
+  date: number
+  time: number
+  text: string
+  switch: boolean
+  dateTimeRange: [number, number]
+  nestObject: {
+    customFormat: number
+  }
 }
 
 export default defineComponent({
   setup() {
-    const columns: ProDescriptionColumns = [
+    const data = ref<DataSource>({
+      date: Date.now(),
+      time: Date.now(),
+      text: '我是一段没用的文本',
+      switch: true,
+      dateTimeRange: [
+        Date.now(),
+        Date.now(),
+      ],
+      nestObject: {
+        customFormat: Date.now(),
+      },
+    })
+
+    const columns: ProDescriptionColumns<DataSource> = [
+      {
+        title: '日期',
+        path: 'date',
+        valueType: 'date',
+        tooltip: '日期',
+      },
+      {
+        title: '时间',
+        path: 'time',
+        valueType: 'time',
+      },
       {
         title: '文本',
         path: 'text',
       },
       {
-        title: '日期时间',
-        path: 'datetime',
-        valueType: 'date-time',
+        title: '开关',
+        path: 'switch',
+        valueType: 'switch',
+      },
+      {
+        title: '日期范围',
+        path: 'dateTimeRange',
+        valueType: 'date-time-range',
+        tooltip: ['日期1', '日期2'],
+      },
+      {
+        title: '自定义格式化时间',
+        path: 'nestObject.customFormat',
+        valueType: 'date',
+        fieldProps: {
+          format: 'yyyy.MM.dd HH.mm.ss',
+        },
       },
     ]
 
-    async function request() {
-      await delay(2000)
-      return {
-        text: '123',
-        datetime: '2024-08-25 22:00:00',
-      }
-    }
-
     return {
+      data,
       columns,
-      request,
     }
   },
 })
 </script>
 
 <template>
-  <pro-card title="描述列表">
-    <pro-descriptions label-placement="left" :request="request" :column="1" :columns="columns" />
-  </pro-card>
+  <pro-descriptions bordered :data="data" :columns="columns" />
 </template>
