d2591194899cf3a13d2b320d8ebf0ebb9eab7b69
zhengchangfu <zcfpromise@163.com>
2024-08-28 23:58:11 +0800

    types(ProDescriptions): 类型优化

--- packages/components/src/descriptions/composables/useProDescriptions.ts
+++ packages/components/src/descriptions/composables/useProDescriptions.ts
@@ -1,24 +1,10 @@
 import { isFunction } from 'lodash-es'
+import type { DescriptionsProps } from 'naive-ui'
 import type { AnyFn } from '../../types'
 import type { ProDescriptionsProps } from '../props'
 import type { ProDescriptionColumns } from '../types'
 import { useProDescriptionsInst } from '../inst'
-import type { UseFetchDataOptions } from './useFetchData'
-
-/**
- * 重写请求类型和 columns、data 类型
- */
-export interface UseProDescriptionsOptions<
-DataSource = void,
-RequestFn extends AnyFn = AnyFn,
-> extends
-  Omit<ProDescriptionsProps, keyof UseFetchDataOptions | 'columns' | 'data'>,
-  UseFetchDataOptions<RequestFn> {
-  data?: DataSource
-  columns?: DataSource extends void
-    ? ProDescriptionColumns<Awaited<ReturnType<RequestFn>>>
-    : ProDescriptionColumns<DataSource>
-}
+import type { UseFetchDataBaseOptions, UseFetchDataOptionsPassedTransform } from './useFetchData'
 
 export type UseProDescriptionsOptionsReturned = [
   ComputedRef<ProDescriptionsProps>,
@@ -27,10 +13,43 @@ export type UseProDescriptionsOptionsReturned = [
   },
 ]
 
-export function useProDescriptions<
-DataSource = void,
-RequestFn extends AnyFn = AnyFn,
->(options: UseProDescriptionsOptions<DataSource, RequestFn> | (() => UseProDescriptionsOptions<DataSource, RequestFn>)): UseProDescriptionsOptionsReturned {
+interface UseProDescriptionsOptionsPassedData<T> extends Omit<DescriptionsProps, 'columns'> {
+  data: T
+  columns?: ProDescriptionColumns<T>
+}
+
+interface UseProDescriptionsOptionsPassedRequest<T extends AnyFn> extends
+  Omit<DescriptionsProps, 'columns'>,
+  UseFetchDataBaseOptions<T> {
+  columns?: ProDescriptionColumns<Awaited<ReturnType<T>> >
+}
+
+interface UseProDescriptionsOptionsPassedRequestTransform<T extends AnyFn, K> extends
+  Omit<DescriptionsProps, 'columns'>,
+  UseFetchDataOptionsPassedTransform<T, K> {
+  columns?: ProDescriptionColumns<Awaited<ReturnType<T>> >
+}
+
+type MaybeFunction<T> = T | (() => T)
+
+export function useProDescriptions<T>(
+  options: MaybeFunction<UseProDescriptionsOptionsPassedData<T>>
+): UseProDescriptionsOptionsReturned
+
+export function useProDescriptions<T extends AnyFn>(
+  options: MaybeFunction<UseProDescriptionsOptionsPassedRequest<T>>
+): UseProDescriptionsOptionsReturned
+
+export function useProDescriptions<T extends AnyFn, K>(
+  options: MaybeFunction<UseProDescriptionsOptionsPassedRequestTransform<T, K>>
+): UseProDescriptionsOptionsReturned
+
+export function useProDescriptions<T, K>(
+  options:
+    | MaybeFunction<UseProDescriptionsOptionsPassedData<T>>
+    | MaybeFunction<UseProDescriptionsOptionsPassedRequest<T extends AnyFn ? T : never>>
+    | MaybeFunction<UseProDescriptionsOptionsPassedRequestTransform<T extends AnyFn ? T : never, K>>,
+): UseProDescriptionsOptionsReturned {
   const [instRef, { reload }] = useProDescriptionsInst()
 
   const resolvedOptions = computed<ProDescriptionsProps>(() => {
