78c471e6777ba965b7c5b5ce91998291f928eba6
zhengchangfu <zcfpromise@163.com>
2024-09-11 18:50:38 +0800

    chore(ProCard): 修改国际化文案 key

--- packages/components/src/card/index.vue
+++ packages/components/src/card/index.vue
@@ -65,8 +65,8 @@ const showSwitchArea = computed(() => {
   return !closable
 })
 
-const expandText = computed(() => {
-  return getMessage('expandText')(show.value)
+const collapseText = computed(() => {
+  return getMessage('collapseText')(!show.value)
 })
 
 function triggerExpand(area: 'main' | 'arrow') {
@@ -153,7 +153,7 @@ const tooltips = computed(() => {
         @click="triggerExpand('arrow')"
       >
         <slot name="switcher" v-bind="{ expanded: show }">
-          <NEl>{{ expandText }}</NEl>
+          <NEl>{{ collapseText }}</NEl>
           <NIcon>
             <component :is="show ? UpOutlined : DownOutlined" />
           </NIcon>
