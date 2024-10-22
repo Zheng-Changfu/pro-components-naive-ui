7c5245778809e22e871efc86e50e80cff877f722
ZhengChangfu <zcfpromise@163.com>
2024-10-22 18:49:54 +0800

    chore: 更新依赖，删除无用代码

--- packages/components/src/data-table/demos/zhCN/basic.demo.vue
+++ packages/components/src/data-table/demos/zhCN/basic.demo.vue
@@ -20,6 +20,12 @@ export default defineComponent({
       {
         title: 'No',
         key: 'no',
+        tooltip: ['123', '123'],
+      },
+      {
+        title: '时间',
+        key: 'now',
+        valueType: 'date-time',
       },
       {
         title: 'Title',
@@ -32,16 +38,16 @@ export default defineComponent({
     ]
 
     const data = [
-      { no: 3, title: 'Wonderwall', length: '4:18' },
-      { no: 4, title: 'Don\'t Look Back in Anger', length: '4:48' },
-      { no: 12, title: 'Champagne Supernova', length: '7:27' },
-      { no: 33, title: 'Wonderwall', length: '4:18' },
-      { no: 44, title: 'Don\'t Look Back in Anger', length: '4:48' },
-      { no: 122, title: 'Champagne Supernova', length: '7:27' },
-      { no: 333, title: 'Wonderwall', length: '4:18' },
-      { no: 4444, title: 'Don\'t Look Back in Anger', length: '4:48' },
-      { no: 1222, title: 'Champagne Supernova', length: '7:27' },
-      { no: 33333, title: 'Wonderwall', length: '4:18' },
+      { now: Date.now(), no: 3, title: 'Wonderwall', length: '4:18' },
+      { now: Date.now(), no: 4, title: 'Don\'t Look Back in Anger', length: '4:48' },
+      { now: Date.now(), no: 12, title: 'Champagne Supernova', length: '7:27' },
+      { now: Date.now(), no: 33, title: 'Wonderwall', length: '4:18' },
+      { now: Date.now(), no: 44, title: 'Don\'t Look Back in Anger', length: '4:48' },
+      { now: Date.now(), no: 122, title: 'Champagne Supernova', length: '7:27' },
+      { now: Date.now(), no: 333, title: 'Wonderwall', length: '4:18' },
+      { now: Date.now(), no: 4444, title: 'Don\'t Look Back in Anger', length: '4:48' },
+      { now: Date.now(), no: 1222, title: 'Champagne Supernova', length: '7:27' },
+      { now: Date.now(), no: 33333, title: 'Wonderwall', length: '4:18' },
     ]
 
     function request() {
