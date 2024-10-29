34749f4ac7b8fb3341e4a6a729941af412134340
zhengchangfu <zcfpromise@163.com>
2024-10-29 21:58:49 +0800

    perf(ProDataTable): 优化 useValueTypeForm 函数创建 form 性能

--- packages/components/src/data-table/index.ts
+++ packages/components/src/data-table/index.ts
@@ -1,6 +1,9 @@
+export { useInjectProDataTableInst } from './context'
 export { default as ProDataTable } from './data-table'
+export * from './inst'
 export * from './props'
 export * from './slots'
+
 export type {
   ProDataTableColumn,
   ProDataTableColumns,
