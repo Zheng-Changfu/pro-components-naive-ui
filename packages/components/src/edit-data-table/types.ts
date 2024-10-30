43720ce5d6d8c892551384ec651ed7c377d61f0a
ZhengChangfu <zcfpromise@163.com>
2024-10-30 15:40:22 +0800

    types(ProEditDataTable): 优化类型，render 函数新增 helpers 参数

--- packages/components/src/edit-data-table/types.ts
+++ packages/components/src/edit-data-table/types.ts
@@ -1,19 +1,31 @@
-import type { TableExpandColumn, TableSelectionColumn } from 'naive-ui/es/data-table/src/interface'
-import type { ProDataTableBaseColumn, ProDataTableColumnGroup, ProDataTableIndexColumn } from '../data-table/types'
+import type { TableSelectionColumn } from 'naive-ui/es/data-table/src/interface'
+import type { VNodeChild } from 'vue'
+import type { ProDataTableBaseColumn, ProDataTableColumnGroup, ProDataTableExpandColumn, ProDataTableIndexColumn } from '../data-table/types'
 import type { ProFieldProps } from '../form'
 
-export interface ProEditDataTableBaseColumn<RowData = any> extends ProDataTableBaseColumn<RowData> {
+interface BaseColumnRenderHelpers {
+  /**
+   * 当前行是否在编辑状态
+   */
+  editable: boolean
+}
+
+export interface ProEditDataTableBaseColumn<RowData = any> extends Omit<ProDataTableBaseColumn<RowData>, 'render'> {
   /**
    * 传递给 ProField 组件的 props，自定义渲染时无效
    */
   proFieldProps?: ProFieldProps | ((rowData: RowData, rowIndex: number) => ProFieldProps)
+  /**
+   * 重写 render 函数
+   */
+  render?: (rowData: RowData, rowIndex: number, helpers: BaseColumnRenderHelpers) => VNodeChild
 }
 
 export type ProEditDataTableColumn<RowData = any> =
-  | ProDataTableIndexColumn<RowData>
-  | TableExpandColumn<RowData>
-  | ProEditDataTableBaseColumn<RowData>
-  | ProDataTableColumnGroup<RowData>
   | TableSelectionColumn<RowData>
+  | ProDataTableIndexColumn<RowData>
+  | ProDataTableColumnGroup<RowData>
+  | ProDataTableExpandColumn<RowData>
+  | ProEditDataTableBaseColumn<RowData>
 
 export type ProEditDataTableColumns<RowData = any> = ProEditDataTableColumn<RowData>[]
