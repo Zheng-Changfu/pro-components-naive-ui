f018884c44ea187280b405daec47e693383ca411
ZhengChangfu <zcfpromise@163.com>
2024-10-30 15:39:47 +0800

    feat(ProEditDataTable): 优化 useColumns 的代码命名及实现

--- packages/components/src/edit-data-table/fields/composables/useColumns.tsx
+++ packages/components/src/edit-data-table/fields/composables/useColumns.tsx
@@ -1,42 +1,45 @@
 import type { ProDataTableColumn } from '../../../data-table'
-import type { ProEditDataTableColumn, ProEditDataTableColumns } from '../../types'
+import type { ProEditDataTableColumns } from '../../types'
 import type { FieldDataTableProps } from '../field-data-table'
 import { mapTree } from 'pro-components-hooks'
 import { ref, watchEffect } from 'vue'
+import { isDragSortColumn } from '../../../data-table/utils/column'
 import FieldDataTableCell from '../field-data-table-cell'
+import { isProEditDataTableBaseColumn } from '../utils/column'
 
 export function useColumns(props: FieldDataTableProps) {
   const columns = ref<ProDataTableColumn[]>([])
 
-  function isSpecialColumn(column: ProEditDataTableColumn) {
-    return ['index', 'expand', 'selection'].includes(column.type as string)
-  }
+  function convertProEditColumnsToProColumns(columns: ProEditDataTableColumns): ProDataTableColumn[] {
+    return mapTree(columns, (column) => {
+      if (
+        !isProEditDataTableBaseColumn(column)
+        || isDragSortColumn(column, props.dragSortKey)
+      ) {
+        return column
+      }
 
-  function resolveColumns(columns: ProEditDataTableColumns) {
-    return mapTree(columns, (column: any) => {
       const columnKey = column.path ?? column.key
-      return isSpecialColumn(column)
-        ? column
-        : {
-            ...column,
-            path: columnKey,
-            render: (row: any, rowIndex: number) => {
-              return (
-                <FieldDataTableCell
-                  row={row}
-                  column={column}
-                  rowIndex={rowIndex}
-                  columnKey={columnKey}
-                />
-              )
-            },
-          }
+      return {
+        ...column,
+        path: column.path ?? column.key,
+        render: (row: any, rowIndex: number) => {
+          return (
+            <FieldDataTableCell
+              row={row}
+              column={column}
+              rowIndex={rowIndex}
+              columnKey={columnKey}
+            />
+          )
+        },
+      }
     }, (props.childrenKey ?? 'children') as any)
   }
 
   watchEffect(() => {
     const values = props.columns ?? []
-    columns.value = resolveColumns(values)
+    columns.value = convertProEditColumnsToProColumns(values)
   })
 
   return {
