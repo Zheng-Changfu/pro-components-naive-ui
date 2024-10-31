6f33a1317cde3bcb24103717433789def576cb57
ZhengChangfu <zcfpromise@163.com>
2024-10-31 19:03:10 +0800

    feat(ProDataTable): 添加 table 插槽，以便自定义控制 table

--- packages/components/src/data-table/slots.ts
+++ packages/components/src/data-table/slots.ts
@@ -1,3 +1,5 @@
+import type { VNodeChild } from 'vue'
+
 export interface ProDataTableSlots {
   /**
    * 表格为空时的内容
@@ -19,6 +21,12 @@ export interface ProDataTableSlots {
    * 表格工具栏
    */
   'toolbar'?: any
+  /**
+   * 自定义表格，一般可以放置一些别的东西
+   */
+  'table'?: {
+    tableDom: VNodeChild
+  }
   /**
    * 自定义查询表单的‘查询’/‘重置’区域
    */
