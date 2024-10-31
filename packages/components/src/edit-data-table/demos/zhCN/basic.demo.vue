652d4f148c9221a2e37fee004a215700ed9e0b5f
ZhengChangfu <zcfpromise@163.com>
2024-10-31 19:06:06 +0800

    docs: 添加示例

--- packages/components/src/edit-data-table/demos/zhCN/basic.demo.vue
+++ packages/components/src/edit-data-table/demos/zhCN/basic.demo.vue
@@ -12,33 +12,40 @@ export default defineComponent({
         title: 'No',
         key: 'no',
         tooltip: ['123', '123'],
+        width: 200,
+        proFieldProps: {
+          required: true,
+        },
       },
       {
         title: '时间',
         key: 'now',
         valueType: 'date-time',
+        width: 200,
       },
       {
         title: 'Title',
         key: 'title',
+        width: 200,
       },
       {
         title: 'Length',
         key: 'length',
+        width: 200,
       },
     ]
 
     const data = [
-      { now: Date.now(), no: 3, title: 'Wonderwall', length: '4:18' },
-      { now: Date.now(), no: 4, title: 'Don\'t Look Back in Anger', length: '4:48' },
-      { now: Date.now(), no: 12, title: 'Champagne Supernova', length: '7:27' },
-      { now: Date.now(), no: 33, title: 'Wonderwall', length: '4:18' },
-      { now: Date.now(), no: 44, title: 'Don\'t Look Back in Anger', length: '4:48' },
-      { now: Date.now(), no: 122, title: 'Champagne Supernova', length: '7:27' },
-      { now: Date.now(), no: 333, title: 'Wonderwall', length: '4:18' },
-      { now: Date.now(), no: 4444, title: 'Don\'t Look Back in Anger', length: '4:48' },
-      { now: Date.now(), no: 1222, title: 'Champagne Supernova', length: '7:27' },
-      { now: Date.now(), no: 33333, title: 'Wonderwall', length: '4:18' },
+      { now: Date.now(), no: '3', title: 'Wonderwall', length: '4:18' },
+      { now: Date.now(), no: '4', title: 'Don\'t Look Back in Anger', length: '4:48' },
+      { now: Date.now(), no: '12', title: 'Champagne Supernova', length: '7:27' },
+      { now: Date.now(), no: '33', title: 'Wonderwall', length: '4:18' },
+      { now: Date.now(), no: '44', title: 'Don\'t Look Back in Anger', length: '4:48' },
+      { now: Date.now(), no: '122', title: 'Champagne Supernova', length: '7:27' },
+      // { now: Date.now(), no: 333, title: 'Wonderwall', length: '4:18' },
+      // { now: Date.now(), no: 4444, title: 'Don\'t Look Back in Anger', length: '4:48' },
+      // { now: Date.now(), no: 1222, title: 'Champagne Supernova', length: '7:27' },
+      // { now: Date.now(), no: 33333, title: 'Wonderwall', length: '4:18' },
     ]
 
     function request() {
@@ -63,15 +70,18 @@ export default defineComponent({
 
 <template>
   <pro-form
-    :initial-value="{
+    :initial-values="{
       list: data,
     }"
-    validateBehavior="popover"
+    validate-behavior="popover"
     @submit="console.log"
   >
     <pro-input title="名称" path="name" />
     <pro-edit-data-table
-      title="爱好" path="list" :columns="columns" :value="data"
+      title="爱好"
+      path="list"
+      :scroll-x="200"
+      :columns="columns"
     />
     <n-button type="primary" attr-type="submit">
       提交
