bfe208201b010f13ffebeefc7123ed97f727f39c
ZhengChangfu <zcfpromise@163.com>
2024-11-04 17:58:10 +0800

    chore(ProDescriptions): 删除无用代码

--- packages/components/src/descriptions/inst.ts
+++ packages/components/src/descriptions/inst.ts
@@ -1,10 +1,6 @@
-import type { PickFunction } from '../types'
-import { createProComponentInstanceFactory } from '../composables'
-
 export interface ProDescriptionsInst {
   /**
    * 重新调用 request
    */
   reload: (params?: any) => Promise<void>
 }
-export const useProDescriptionsInst = createProComponentInstanceFactory<PickFunction<ProDescriptionsInst>>('ProDescriptions')
