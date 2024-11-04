7c25fd65b2e2c654b4bf03219ee5461cc6ec1a9f
ZhengChangfu <zcfpromise@163.com>
2024-11-04 15:09:27 +0800

    types: 删除 PickFunction

--- packages/components/src/types/index.ts
+++ packages/components/src/types/index.ts
@@ -2,9 +2,6 @@ import type { CSSProperties } from 'vue'
 
 export type AnyFn = (...args: any[]) => any
 export type IsFunction<T> = T extends AnyFn ? true : false
-export type PickFunction<T extends Record<string, any>> = {
-  [K in keyof T as IsFunction<T[K]> extends true ? K : never]: T[K]
-}
 export type ExtendAttrsStyleProps<T> = T & {
   class?: string
   style?: CSSProperties
