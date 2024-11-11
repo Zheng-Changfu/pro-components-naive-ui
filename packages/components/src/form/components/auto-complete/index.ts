3990acfea3b04c85e626a15e8def47df75aa9583
ZhengChangfu <zcfpromise@163.com>
2024-11-11 17:59:53 +0800

    eslint: fix

--- packages/components/src/form/components/auto-complete/index.ts
+++ packages/components/src/form/components/auto-complete/index.ts
@@ -1,4 +1,4 @@
 export { default as ProAutoComplete } from './auto-complete'
-export type{ ProAutoCompleteInst } from './inst'
+export type { ProAutoCompleteInst } from './inst'
 export * from './props'
 export * from './slots'
--- packages/components/src/form/components/upload/index.ts
+++ packages/components/src/form/components/upload/index.ts
@@ -1,4 +1,4 @@
-export type{ ProUploadInst } from './inst'
+export type { ProUploadInst } from './inst'
 export * from './props'
 export * from './slots'
 export { default as ProUpload } from './upload'
