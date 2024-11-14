52851354eeef0f0ce202d40c23596a50c225ba9d
ZhengChangfu <zcfpromise@163.com>
2024-11-14 16:09:16 +0800

    chore: 移除无用代码

--- packages/components/src/config-provider/index.ts
+++ packages/components/src/config-provider/index.ts
@@ -1,4 +1,4 @@
+export * from './components/form-clearable-provider'
 export { default as ProConfigProvider } from './config-provider'
 export * from './context'
 export * from './props'
-export * from './types'
