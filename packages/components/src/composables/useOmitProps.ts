4143ac651f000be8b99d3d85bab43f50e1558cc8
zhengchangfu <zcfpromise@163.com>
2024-11-17 13:05:57 +0800

    types(useOmitProps): 优化类型

--- packages/components/src/composables/useOmitProps.ts
+++ packages/components/src/composables/useOmitProps.ts
@@ -1,3 +1,4 @@
+import type { Simplify } from 'type-fest'
 import type { UnwrapRef } from 'vue'
 import { omit } from 'lodash-es'
 import { computed, unref } from 'vue'
@@ -5,6 +6,6 @@ import { computed, unref } from 'vue'
 export function useOmitProps<T extends object, K extends keyof UnwrapRef<T>>(props: T, excludeProps: Record<K, any>) {
   const excludePropKeys = Object.keys(excludeProps)
   return computed(() => {
-    return omit(unref(props), excludePropKeys) as Partial<Omit<UnwrapRef<T>, K>>
+    return omit(unref(props), excludePropKeys) as unknown as Simplify<Partial<Omit<UnwrapRef<T>, K>>>
   })
 }
