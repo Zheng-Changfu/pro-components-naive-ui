fdca9311b6ae0044965b3c3060b9ea0679d7c50e
ZhengChangfu <zcfpromise@163.com>
2024-11-18 17:04:24 +0800

    site: 添加 __DEV__  环境变量支持

--- vite.config.ts
+++ vite.config.ts
@@ -4,57 +4,63 @@ import UnoCSS from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 // import { analyzer } from 'vite-bundle-analyzer'
 import vitePluginDemo from './build/vite-plugin-demo'
 
-export default defineConfig({
-  optimizeDeps: {
-    include: [
-      'seemly',
+export default defineConfig(({ mode }) => {
+  const nodeEnv = loadEnv(mode, './').VITE_USER_NODE_ENV
+  return {
+    optimizeDeps: {
+      include: [
+        'seemly',
+      ],
+    },
+    define: {
+      __DEV__: nodeEnv !== 'production',
+    },
+    plugins: [
+      UnoCSS(),
+      ...vitePluginDemo(),
+      vueJsx({}),
+      AutoImport({
+        imports: [
+          'vue',
+          {
+            'naive-ui': [
+              'useDialog',
+              'useMessage',
+              'useNotification',
+              'useLoadingBar',
+            ],
+          },
+        ],
+      }),
+      Components({
+        resolvers: [NaiveUiResolver()],
+      }),
+      // analyzer(),
     ],
-  },
-  plugins: [
-    UnoCSS(),
-    ...vitePluginDemo(),
-    vueJsx({}),
-    AutoImport({
-      imports: [
-        'vue',
+    resolve: {
+      alias: [
         {
-          'naive-ui': [
-            'useDialog',
-            'useMessage',
-            'useNotification',
-            'useLoadingBar',
-          ],
+          find: 'pro-components-naive-ui',
+          replacement: resolve(__dirname, './packages/components/index.ts'),
+        },
+        {
+          find: '/demo',
+          replacement: resolve(__dirname, './src'),
         },
       ],
-    }),
-    Components({
-      resolvers: [NaiveUiResolver()],
-    }),
-    // analyzer(),
-  ],
-  resolve: {
-    alias: [
-      {
-        find: 'pro-components-naive-ui',
-        replacement: resolve(__dirname, './packages/components/index.ts'),
-      },
-      {
-        find: '/demo',
-        replacement: resolve(__dirname, './src'),
-      },
-    ],
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'naive-ui': ['naive-ui'],
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            'naive-ui': ['naive-ui'],
+          },
         },
       },
     },
-  },
+  }
 })
