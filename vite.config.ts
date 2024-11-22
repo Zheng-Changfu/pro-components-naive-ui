932fd35bdd153f61beb17f8d7fac58daa4989249
ZhengChangfu <zcfpromise@163.com>
2024-11-22 16:39:06 +0800

    build: 优化打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
 import { defineConfig, loadEnv } from 'vite'
+import { ProComponentsNaiveUiResolver } from './packages/resolvers/index'
 // import { analyzer } from 'vite-bundle-analyzer'
 import vitePluginDemo from './build/vite-plugin-demo'
 
@@ -37,7 +38,10 @@ export default defineConfig(({ mode }) => {
         ],
       }),
       Components({
-        resolvers: [NaiveUiResolver()],
+        resolvers: [
+          NaiveUiResolver(),
+          ProComponentsNaiveUiResolver(),
+        ],
       }),
       // analyzer(),
     ],
