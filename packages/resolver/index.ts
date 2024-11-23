8770ddad6bc5c2f2b58871f43b0570c5c9f93cd0
zhengchangfu <zcfpromise@163.com>
2024-11-23 13:28:45 +0800

    chore(resolve): ProComponentsNaiveUiResolver 更名为 ProNaiveUiResolver

--- packages/resolver/index.ts
+++ packages/resolver/index.ts
@@ -1,6 +1,6 @@
 import type { ComponentResolver } from 'unplugin-vue-components'
 
-export function ProComponentsNaiveUiResolver(): ComponentResolver {
+export function ProNaiveUiResolver(): ComponentResolver {
   return {
     type: 'component',
     resolve: (name: string) => {
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 import Components from 'unplugin-vue-components/vite'
 import { defineConfig, loadEnv } from 'vite'
-import { ProComponentsNaiveUiResolver } from './packages/resolver/index'
+import { ProNaiveUiResolver } from './packages/resolver/index'
 // import { analyzer } from 'vite-bundle-analyzer'
 import vitePluginDemo from './build/vite-plugin-demo'
 
@@ -40,7 +40,7 @@ export default defineConfig(({ mode }) => {
       Components({
         resolvers: [
           NaiveUiResolver(),
-          ProComponentsNaiveUiResolver(),
+          ProNaiveUiResolver(),
         ],
       }),
       // analyzer(),
