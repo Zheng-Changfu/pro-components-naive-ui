1a2c7a2ac7eb13e62592d72d1b31b41dea60a76e
ZhengChangfu <zcfpromise@163.com>
2024-12-03 17:08:21 +0800

    chore(use-omit-props): omit 替换为 simplyOmit

--- packages/components/src/composables/useOmitProps.ts
+++ packages/components/src/composables/useOmitProps.ts
@@ -1,11 +1,18 @@
 import type { Simplify } from 'type-fest'
 import type { UnwrapRef } from 'vue'
-import { omit } from 'lodash-es'
 import { computed, unref } from 'vue'
+import { keysOf } from '../_utils/keysOf'
+import { simplyOmit } from '../_utils/simplyOmit'
 
-export function useOmitProps<T extends object, K extends keyof UnwrapRef<T>>(props: T, excludeProps: Record<K, any>) {
-  const excludePropKeys = Object.keys(excludeProps)
+export function useOmitProps<T extends object, K extends keyof UnwrapRef<T>>(
+  props: T,
+  excludeProps: Record<K, any>,
+) {
+  const excludePropKeys = keysOf(excludeProps)
   return computed(() => {
-    return omit(unref(props), excludePropKeys) as unknown as Simplify<Partial<Omit<UnwrapRef<T>, K>>>
+    return simplyOmit(
+      unref(props),
+      excludePropKeys as any,
+    ) as unknown as Simplify<Partial<Omit<UnwrapRef<T>, K>>>
   })
 }
