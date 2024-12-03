72256095b57bc051ac651a96a4e5b612ffc1f810
zhengchangfu <zcfpromise@163.com>
2024-12-04 00:05:14 +0800

    chore(field): readonly、showLabel、validateBehavior、validateBehaviorProps 可以从 ListField 中注入

--- packages/components/src/form/components/field/context.ts
+++ packages/components/src/form/components/field/context.ts
@@ -1,6 +1,11 @@
+import type { PopoverProps } from 'naive-ui'
 import type { MaybeRef } from 'vue'
+import type { ValidateBehavior } from '../../props'
 import { createInjectionKey } from '../../../composables/createInjectionKey'
 
 export const proFieldConfigInjectionKey = createInjectionKey<{
-  showLabel: MaybeRef<boolean | undefined>
+  readonly?: MaybeRef<boolean | undefined>
+  showLabel?: MaybeRef<boolean | undefined>
+  validateBehavior?: MaybeRef<ValidateBehavior | undefined>
+  validateBehaviorProps?: MaybeRef<PopoverProps | undefined>
 }>('pro-field-config')
