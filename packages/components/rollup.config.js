0dcda6623068a5046c1447161e21adff1bdf1792
zhengchangfu <zcfpromise@163.com>
2024-12-16 01:54:38 +0800

    build: rollup 构建组件库

--- packages/components/rollup.config.js
+++ packages/components/rollup.config.js
@@ -1,63 +1,67 @@
 import path from 'node:path'
 import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
 import resolve from '@rollup/plugin-node-resolve'
 import replace from '@rollup/plugin-replace'
 import terser from '@rollup/plugin-terser'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import mergeConfig from 'deepmerge'
-// import dts from 'rollup-plugin-dts'
-import typescript from 'rollup-plugin-typescript2'
-// import vue from 'rollup-plugin-vue'
+import { dts } from 'rollup-plugin-dts'
+import e from 'rollup-plugin-esbuild'
 
-const extensions = ['.mjs', '.js', '.json', '.ts', '.tsx']
+const esbuild = e.default
+
+const extensions = ['.mjs', '.js', '.json', '.ts']
 
 // 公共配置
 const baseConfig = {
-  input: 'index.ts',
+  input: 'src/index.ts',
   external: ['vue', 'naive-ui'],
   plugins: [
-    // vue(),
+    json(),
     resolve({ extensions }),
+    esbuild({
+      tsconfig: path.resolve(__dirname, '../../tsconfig.json'),
+      target: 'esnext',
+      sourceMap: true,
+    }),
+    babel({
+      babelHelpers: 'bundled',
+      extensions,
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: '> 0.5%, last 2 versions, not dead', // 支持最新两个版本的现代浏览器
+          },
+        ],
+      ],
+    }),
     commonjs(),
     vueJsx(),
-    typescript({
-      tsconfig: path.resolve(__dirname, '../../tsconfig.json'),
-    }),
-    // babel({
-    //   babelHelpers: 'bundled',
-    //   extensions,
-    //   presets: [
-    //     [
-    //       '@babel/preset-env',
-    //       {
-    //         targets: '> 0.5%, last 2 versions, not dead', // 支持最新两个版本的现代浏览器
-    //       },
-    //     ],
-    //   ],
-    // }),
   ],
 }
 
 // es 和 lib 的配置
-// function esAndLibConfig(dir, format) {
-//   return {
-//     output: {
-//       dir,
-//       format,
-//       preserveModules: true,
-//       preserveModulesRoot: 'src',
-//       entryFileNames: '[name].js',
-//       sourcemap: true,
-//     },
-//     plugins: [
-//       replace({
-//         __DEV__: 'process.env.NODE_ENV !== "production"',
-//         preventAssignment: true,
-//       }),
-//     ],
-//   }
-// }
+function esAndLibConfig(dir, format) {
+  return {
+    output: {
+      dir,
+      format,
+      preserveModules: true,
+      preserveModulesRoot: path.resolve(__dirname, 'src'),
+      entryFileNames: '[name].js',
+      exports: format === 'cjs' ? 'named' : undefined,
+    },
+    plugins: [
+      replace({
+        __DEV__: 'process.env.NODE_ENV !== "production"',
+        preventAssignment: true,
+      }),
+    ],
+  }
+}
 
 // umd 和 esm 的开发和生产环境配置
 function umdEsmConfig(file, format, isProd) {
@@ -66,7 +70,11 @@ function umdEsmConfig(file, format, isProd) {
       file,
       format,
       name: format === 'umd' ? 'proNaive' : undefined,
-      sourcemap: true,
+      exports: format === 'umd' ? 'named' : undefined,
+      globals: {
+        'vue': 'Vue',
+        'naive-ui': 'naive',
+      },
     },
     plugins: [
       replace({
@@ -81,30 +89,45 @@ function umdEsmConfig(file, format, isProd) {
   }
 }
 
-// DTS 文件配置
-// const dtsConfig = {
-//   input: 'src/index.ts',
-//   output: [
-//     {
-//       file: 'dist/es/index.d.ts',
-//       format: 'esm',
-//     },
-//     {
-//       file: 'dist/lib/index.d.ts',
-//       format: 'cjs',
-//     },
-//   ],
-//   plugins: [dts()],
-// }
+// dts 文件配置
+const dtsConfig = {
+  input: 'src/index.ts',
+  output: [
+    {
+      dir: 'es',
+      format: 'esm',
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+      entryFileNames: '[name].d.ts',
+    },
+    {
+      dir: 'lib',
+      format: 'cjs',
+      preserveModules: true,
+      preserveModulesRoot: 'src',
+      entryFileNames: '[name].d.ts',
+    },
+  ],
+  plugins: [
+    json(),
+    dts({
+      tsconfig: path.resolve(__dirname, '../../tsconfig.json'),
+      compilerOptions: {
+        preserveSymlinks: false,
+      },
+    }),
+  ],
+}
 
 export default [
-  // mergeConfig(baseConfig, esAndLibConfig('es', 'esm')), // es 目录
+  mergeConfig(baseConfig, esAndLibConfig('es', 'esm')), // es 目录
   // mergeConfig(baseConfig, esAndLibConfig('lib', 'cjs')), // lib 目录
+
+  // mergeConfig(baseConfig, umdEsmConfig('dist/index.js', 'umd', false)), // 开发环境 umd
+  // mergeConfig(baseConfig, umdEsmConfig('dist/index.prod.js', 'umd', true)), // 生产环境 umd
+
+  // mergeConfig(baseConfig, umdEsmConfig('dist/index.mjs', 'esm', false)), // 开发环境 esm
+  // mergeConfig(baseConfig, umdEsmConfig('dist/index.prod.mjs', 'esm', true)), // 生产环境 esm
+
   // dtsConfig, // .d.ts 文件
-
-  mergeConfig(baseConfig, umdEsmConfig('dist/index.js', 'umd', false)), // 开发环境 umd
-  mergeConfig(baseConfig, umdEsmConfig('dist/index.prod.js', 'umd', true)), // 生产环境 umd
-
-  mergeConfig(baseConfig, umdEsmConfig('dist/index.mjs', 'esm', false)), // 开发环境 esm
-  mergeConfig(baseConfig, umdEsmConfig('dist/index.prod.mjs', 'esm', true)), // 生产环境 esm
 ]
