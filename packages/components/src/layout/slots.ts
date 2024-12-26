97def5ecc1aaf53fcef8883b2967dd823e6f2e5c
Zheng-Changfu <zcfpromise@163.com>
2024-12-26 16:15:40 +0800

    feat: add ProLayoutSlots interface for layout slot definitions

---
+++ packages/components/src/layout/slots.ts
@@ -0,0 +1,13 @@
+export interface ProLayoutSlots {
+  'logo': any
+  'menu': any
+  'extra': any
+  'header': any
+  'tabbar': any
+  'footer': any
+  'content': any
+  'mixed-menu': any
+  'side-extra': any
+  'content-overlay': any
+  'side-extra-title': any
+}
